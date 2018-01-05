52e96a2a0e6ddcbb596382fb690472385b6a27f0
Emma Orhun <emmaorhun@shopify.com>
2018-01-05 00:25:43 -0500

    Adding a button for my resume

---
+++ siteScript.js
@@ -0,0 +1,16 @@
+var theater = theaterJS()
+
+theater
+  .on('type:start, erase:start', function () {
+    theater.getCurrentActor().$element.classList.add('actor__content--typing')
+  })
+  .on('type:end, erase:end', function () {
+    theater.getCurrentActor().$element.classList.remove('actor__content--typing')
+  })
+  .on('type:start, erase:start', function () {
+    if (theater.getCurrentActor().name === 'vader') {
+      document.body.classList.add('dark')
+    } else {
+      document.body.classList.remove('dark')
+    }
+  })
